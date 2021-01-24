9527e18911cb89e500f221e4abe4f4a36f73749d
HungryBurger <hjjooace24@gmail.com>
2021-01-24 22:52:53 +0900

    Entity Class 2-1

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,11 +4,9 @@ package jpabook.jpashop.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.jnlp.UnavailableServiceException;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Entity
